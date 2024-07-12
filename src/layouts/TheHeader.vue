4178ce1d944b7fb41eafabffd7066109179eca40
je.noh <je.noh@theuber.co.kr>
2024-07-12 16:42:59 +0900

    vue router - 03

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -17,6 +17,9 @@
                         <li class="nav-item">
                             <RouterLink class="nav-link" active-class="active" to="/posts">게시글</RouterLink>
                         </li>
+                        <li class="nav-item">
+                            <RouterLink class="nav-link" active-class="active" to="/nested">Nested</RouterLink>
+                        </li>
                     </ul>
                     <div class="d-flex" role="search">
                         <button class="btn btn-outline-success" type="button" @click="goPage">글쓰기</button>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,11 @@ import PostCreateView from "@/views/posts/PostCreateView.vue";
 import PostListView from "@/views/posts/PostListView.vue";
 import PostDetailView from "@/views/posts/PostDetailView.vue";
 import PostEditView from "@/views/posts/PostEditView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
+import NestedView from "@/views/nested/NestedView.vue";
+import NestedOneView from "@/views/nested/NestedOneView.vue";
+import NestedTwoView from "@/views/nested/NestedTwoView.vue";
+import NestedHomeView from "@/views/nested/NestedHomeView.vue";
 
 const routes = [
     {
@@ -37,6 +42,33 @@ const routes = [
         name: 'PostEdit',
         component: PostEditView
     },
+    {
+        path: '/nested',
+        name: 'Nested',
+        component: NestedView,
+        children: [
+            {
+                path: '',
+                name: 'NestedHome',
+                component: NestedHomeView
+            },
+            {
+                path: 'one',
+                name: 'NestedOne',
+                component: NestedOneView
+            },
+            {
+                path: 'two',
+                name: 'NestedTwo',
+                component: NestedTwoView
+            }
+        ]
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: NotFoundView
+    }
 ]
 
 const router = createRouter({
